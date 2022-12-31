e43bfb3b764bdb042bce7ab2ebe8ec351882c6b6
dhq_boiler <dhq_boiler@live.jp>
2022-12-31 22:22:31 +0900

    step 1

---
+++ Article20230105/BlazorApp1/BlazorApp1/Client/wwwroot/js/module.js
@@ -0,0 +1,19 @@
+﻿var temp;
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function push(val) {
+    temp = val;
+}
+function pop() {
+    return temp;
+}
