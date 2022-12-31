615e9ebcfe63fd13c1ac8e74d7be42742d3a2d14
dhq_boiler <dhq_boiler@live.jp>
2022-12-31 22:50:52 +0900

    step 4

--- Article20230105/BlazorApp1/BlazorApp1/Client/wwwroot/js/module.js
+++ Article20230105/BlazorApp1/BlazorApp1/Client/wwwroot/js/module.js
@@ -1,19 +1,21 @@
-﻿temp;
-add(a, b) {
-    return a + b;
-}
-subtract(a, b) {
-    return a - b;
-}
-multiply(a, b) {
-    return a * b;
-}
-divide(a, b) {
-    return a / b;
-}
-push(val) {
-    temp = val;
-}
-pop() {
-    return temp;
+﻿class Module {
+    temp;
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        return a / b;
+    }
+    push(val) {
+        temp = val;
+    }
+    pop() {
+        return temp;
+    }
 }
